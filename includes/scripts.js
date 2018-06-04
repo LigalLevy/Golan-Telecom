7dd0aabd554d6ef3971bfe3f3ba25c9f6d09c6b6
LigalLevy <37476027+LigalLevy@users.noreply.github.com>
2018-06-05 01:55:09 +0300

    JS for modal

--- includes/scripts.js
+++ includes/scripts.js
@@ -13,7 +13,9 @@ $(function() {
 $(function() {
     $('#nav-list li').click(function() {
         $('#nav-list li').removeClass('selected-nav');
+        $('#nav-list p').removeAttr('id','p-hover');
         $(this).addClass('selected-nav');
+        $(this).children().first().attr('id','p-hover');
 
     });
 });
@@ -36,12 +38,58 @@ for (i = 0; i < acc.length; i++) {
     });
 }
 
-// Open article
-function openArticle() {
-    document.getElementById('myModal').style.display = "block";
+/*SMOOTH ANIMATE*/
+$(document).ready(function(){
+    // Add smooth scrolling to all links
+    $("a").on('click', function(event) {
+
+        // Make sure this.hash has a value before overriding default behavior
+        if (this.hash !== "") {
+            // Prevent default anchor click behavior
+            event.preventDefault();
+
+            // Store hash
+            var hash = this.hash;
+
+            // Using jQuery's animate() method to add smooth page scroll
+            // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+            $('html, body').animate({
+                scrollTop: $(hash).offset().top
+            }, 800, function(){
+
+                // Add hash (#) to URL when done scrolling (default click behavior)
+                window.location.hash = hash;
+            });
+        } // End if
+    });
+});
+
+
+/*MODAL ARTICLES*/
+// Get the modal
+var modal = document.getElementById('myModal');
+
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks the button, open the modal
+btn.onclick = function() {
+    modal.style.display = "block";
+    span.style.display = "block";
 }
 
-// Close article
-function closeArticle() {
-    document.getElementById('myModal').style.display = "none";
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+    modal.style.display = "none";
+    span.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
 }
